dea7d55574e54dda640628f1445bac9a9060e49f
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 19:25:35 +0000

    created validates for pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,21 @@
 class Pet < ActiveRecord::Base
+  self.inheritance_column = nil
+
+  belongs_to :user
+
+  attr_accessible :name, :type, :breed, :age, :weight, :date
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 35 }
+  validates :type, presence: true
+  validate :type_is_dog_or_cat
+  validates :breed, presence: true
+  validates :breed, length: { maximum: 35 }
+  validates :age, presence: true
+  validates :weight, presence: true
+  validats :date, presence: true
+
+  def type_is_dog_or_cat
+    type == 'dog' || type == 'cat'
+  end
 end
