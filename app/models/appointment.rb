8dde95708804f5cade488d469e213c4787851044
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 19:05:11 +0000

    created appointments controller

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,9 +7,9 @@ class Appointment < ActiveRecord::Base
 
   validates :date, presence: true
   validate :date_cannot_be_in_the_past
-  validates :pet, :presence: true
-  validates :customer, :presence: true
-  validates :reason, :presence: true 
+  validates :pet, presence: true
+  validates :customer, presence: true
+  validates :reason, presence: true 
 
   def date_cannot_be_in_the_past
     errors.add(:date, "can't be in the past") if
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rackspace::Application.routes.draw do
+  resources :appointments
+
   devise_for :users, controllers: { registrations: "registrations" }
 end
