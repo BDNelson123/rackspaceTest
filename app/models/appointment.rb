7547287ef57fbaaa00a401a4f4b2ffaa2ef4a231
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 18:35:27 +0000

    created model for appointments and added validations

---
+++ app/models/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment < ActiveRecord::Base
+  self.inheritance_column = nil
+
+  belongs_to :user
+
+  attr_accessible :date, :pet, :customer, :reminder, :reason
+
+  validates :date, presence: true
+  validate :date_cannot_be_in_the_past
+  validates :pet, :presence: true
+  validates :customer, :presence: true
+  validates :reason, :presence: true 
+
+  def date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past") if
+      expiration_date < Date.today
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   self.inheritance_column = nil
 
+  has_many :appointments, dependant: :destroy
+  has_many :pets, dependant: :destroy
+
   attr_accessible :name, :years, :address, :city, :state, :zip, :school, :years, :type, :code, :email, :password, :password_confirmation 
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
