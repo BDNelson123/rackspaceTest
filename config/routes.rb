a47d97444049f0acd951a1a67b568a6891b387a7
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 22:06:36 +0000

    put in members to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rackspace::Application.routes.draw do
   resources :pets
-
   resources :appointments
+  resources :members
 
   devise_for :users, controllers: { registrations: "registrations" }
 end
