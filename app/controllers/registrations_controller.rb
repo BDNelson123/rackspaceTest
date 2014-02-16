a3a073f5e9a2f0715ad5ea8bf2fab1779d55f780
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 00:43:02 +0000

    fixed issue with string being compared to integer for user registration from codes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    @code = Code.select('code').where(:type => params['user']['type']).first
+    @user = User.new(params['user'])
+
+    if params['user']['code'].to_i == @code[:code].to_i
+      @user.save
+      redirect_to new_user_session_path
+    else
+      flash[:alert] = 'Your code to register was incorrect.'
+      render :action => "new"
+    end
+  end
 end
