4f4355c2cb95c3efcb79a38526c2a53a6002deae
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 22:40:36 +0000

    comments to application controller, made members index now show customers, owners, receptionists, and veterinarians depending on param, and small changes to header

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,14 +2,14 @@ class MembersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    if current_user.type == "Owner"
-      @users = User.where("type = ? or type = ?", "Veterinarian", "Owner")
-    elsif current_user.type == "Veterinarian"
-      @users = User.where("type = ? or type = ?", "Veterinarian", "Owner")
-    elsif current_user.type == "Receptionist"
+    if params['type'] == nil || params['type'] == "Customer"
       @users = User.where(:type => "Customer")
-    else
-      redirect_to(:controller => 'appointments', :action => 'show', :id => current_user.id)
+    elsif params['type'] == "Receptionist"
+      @users = User.where(:type => "Receptionist")
+    elsif params['type'] == "Veterinarian"
+      @users = User.where(:type => "Veterinarian")
+    elsif params['type'] == "Owner"
+      @users = User.where(:type => "Owner")
     end
   end
 end
