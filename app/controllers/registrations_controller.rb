62de5dbf91b33836c6dfe28aa8112e625e020d41
BDNelson123 <bnelson@parallelsolutionsinc.net>
2014-02-16 00:50:29 +0000

    changed redirect after registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     if params['user']['code'].to_i == @code[:code].to_i
       @user.save
-      redirect_to new_user_session_path
+      redirect_to after_sign_in_path_for(resource)
     else
       flash[:alert] = 'Your code to register was incorrect.'
       render :action => "new"
